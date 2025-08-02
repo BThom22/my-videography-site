64e26a36c70a5797eada501253aeb3c5cbb72e96
BThom22 <rbthomas181@gmail.com>
2025-08-01 20:10:51 -0500

    hamburger

---
+++ main.js
@@ -0,0 +1,12 @@
+// menu.js
+document.addEventListener("DOMContentLoaded", function() {
+  var hamburger = document.querySelector('.hamburger');
+  var navLinks = document.querySelector('.nav-links');
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener('click', function() {
+      navLinks.classList.toggle('active');
+    });
+  }
+});
+
