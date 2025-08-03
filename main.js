2534bbb215b19948829b8ec1fa5b30b71398316b
BThom22 <rbthomas181@gmail.com>
2025-08-03 11:02:49 -0500

    changes

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.querySelector('.hamburger');
   const navLinks = document.querySelector('.nav-links');
+
   hamburger.addEventListener('click', function() {
     navLinks.classList.toggle('active');
-    hamburger.classList.toggle('open'); // for hamburger icon animation if you want
+    hamburger.classList.toggle('open');
   });
 });
 
